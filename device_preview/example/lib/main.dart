1e7c763fb6addb1706e86527abc5fd38f91c16cb
Vlas Artanov <vlas.artanov@privatbank.ua>
2025-05-20 11:42:55 +0300

    Update LICENSE and remove README; add locale support in example app

--- device_preview/example/lib/main.dart
+++ device_preview/example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +15,23 @@ void main() {
           enabled: true,
           padding: EdgeInsets.zero,
           safeAreaBottomPadding: bottomPadding,
+          availableLocales: const [
+            Locale('uk'),
+            Locale('en'),
+            Locale('de'),
+          ],
+          onEnabledToggle: (p0, state) {
+            print('Enabled: $p0');
+            if (!p0) {
+              //storage?.save(DevicePreviewData(locale: 'uk'));
+              //storage?.load();
+              // state.data = state.data.copyWith(
+              //   isDarkMode: false,
+              //   locale: 'uk',
+              // );
+            }
+          },
+          onChangeLanguageToggle: (p0) => print('Change language: $p0'),
           builder: (context) => const BasicApp(),
         );
       },
